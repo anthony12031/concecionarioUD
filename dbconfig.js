135bc0b2b9782a65a8260b76082c75470f711655
Anthony Vargas <tony_jason@hotmail.com>
2017-11-26 21:46:49 -0500

    instalacion de paquetes necesarios

---
+++ dbconfig.js
@@ -0,0 +1,5 @@
+module.exports = {
+  user          : "system",
+  password      : "juegosxbox",
+  connectString : "localhost/XE"
+};
---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+
+
+app.use('/',express.static('front-end'));
+
+
+
+app.get('/', function (req, res) {
+  res.send('server running');
+});
+
+var port = process.env.PORT || 8282;
+
+app.listen(port, function () {
+  console.log('app listening on port '+port);
+});
